fc09c92582e9e506e6451689dcc97b8b61747978
Nelson Ganga <nelson.ganga.inacap@outlook.com>
2019-04-25 11:59:14 -0400

    App Académica

---
+++ AcademicaApp/src/academicaapp/AcademicaApp.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package academicaapp;
+
+import menu.MenuPrincipal;
+
+/**
+ *
+ * @author fredy
+ */
+public class AcademicaApp {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //Iniciamos el menu principal
+        MenuPrincipal menuPrincipal = new MenuPrincipal();
+        menuPrincipal.setVisible(true);
+    }
+    
+}
